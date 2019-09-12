d500f33c06bf522e164f0a700e2812f10eaf760b
Rishi Shah <rishishah200@gmail.com>
2019-09-11 21:27:39 -0400

    Added distances
    Need to add interval and part b. of the question

--- Inner Classes/Jpanel - Anonymous Inner Classes/Mousetracker.java
+++ Inner Classes/Jpanel - Anonymous Inner Classes/Mousetracker.java
@@ -1,47 +1,51 @@
 import java.awt.event.*;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
+import java.util.ArrayList;
 
 public class Mousetracker extends JPanel{
 
 	int x = 100, y = 100;
-	int initial = 0, end = 0;
+	double total = 0.0;
+	int initX, initY, newX, newY;
 
-	public Mousetracker(){
+	public Mousetracker(double pix, double in){
 
 		JFrame frame = new JFrame();
 		frame.add(this);
-		MouseMotionListener mouse = new Mouseinterface();
-		addMouseMotionListener(mouse);
 		frame.setVisible(true);
 		setFocusable(true);
+		addMouseMotionListener(new MouseMotionListener(){
+
+			public void mouseMoved(MouseEvent e){
+				newX = e.getX();
+				newY = e.getY();
+
+				double dist = Math.sqrt(Math.pow(newX - initX, 2) + Math.pow(newY - initY, 2)) * ((in / 12.0) / pix);
+				total+=dist;
+				System.out.println(total);
+
+				initX = e.getX();
+				initY = e.getY();
+
+
+
+			}
+
+			public void mouseDragged(MouseEvent e){}
+
+
+		});
 
 	}
 
 
 	public static void main(String[]args){
 
-	Mousetracker track = new Mousetracker();
+	Mousetracker track = new Mousetracker(1600.0,16);
 
 	}
 
-	public class Mouseinterface implements MouseMotionListener{
 
-		public void mouseExited(MouseEvent e){}
-		public void mouseEntered(MouseEvent e){}
-		public void mouseReleased(MouseEvent e){
-		}
-		public void mousePressed(MouseEvent e){}
-		public void mouseClicked(MouseEvent e){
-		}
-		public void mouseMoved(MouseEvent e){}
-		public void mouseDragged(MouseEvent e){
-			x = e.getX();
-			y = e.getY();
-			System.out.println("X"+x);
-			System.out.println("Y"+y);
-		}
-
-	}
 
 }
