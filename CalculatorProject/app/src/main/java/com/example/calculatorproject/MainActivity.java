fb4ac57b8d53b03e33cf62097429f1729639a6af
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2019-10-10 13:15:55 -0400

    Updated Layout

--- CalculatorProject/app/src/main/java/com/example/calculatorproject/MainActivity.java
+++ CalculatorProject/app/src/main/java/com/example/calculatorproject/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.calculatorproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
