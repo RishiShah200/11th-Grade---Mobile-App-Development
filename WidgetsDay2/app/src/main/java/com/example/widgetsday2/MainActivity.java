c422200a1da0872f218dc5a3fbd15fb57aab825e
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2019-10-30 13:25:59 -0400

    WidgetsDay2

---
+++ WidgetsDay2/app/src/main/java/com/example/widgetsday2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.widgetsday2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.id_radiogroup);
+        textView = findViewById(R.id.textView);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(checkedId == R.id.A)
+                    textView.setText("Selection A");
+                if(checkedId == R.id.B)
+                    textView.setText("Selection B");
+                if(checkedId == R.id.C)
+                    textView.setText("Selection C");
+
+                Toast myToast = Toast.makeText(MainActivity.this, "C is the best",Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+    }
+}
