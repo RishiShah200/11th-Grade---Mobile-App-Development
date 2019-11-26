43911db6b097153ebfbf89e1db1a596ce2e3a8f7
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2019-11-26 13:45:35 -0500

    LifeCycleDemo

---
+++ LifeCycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lifecycledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button       = findViewById(R.id.id_button);
+        textView     = findViewById(R.id.id_text);
+    }
+}
