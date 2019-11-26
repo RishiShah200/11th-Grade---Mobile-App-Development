e8e950b755592c15726b59a8909310090d605b82
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2019-11-26 13:49:22 -0500

    LifeCycleDemo

--- LifeCycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.java
+++ LifeCycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lifecycledemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
     TextView textView;
-
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         button       = findViewById(R.id.id_button);
         textView     = findViewById(R.id.id_text);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                textView.setText("Count: " + counter);
+            }
+        });
+
+
     }
 }
