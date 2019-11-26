24afb5cd6081e002f3d6c7fc8dabb2f14efc9c11
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2019-11-26 13:35:44 -0500

    Update MainActivity.java

--- ListViewsAndLifeCycle/app/src/main/java/com/example/listviewsandlifecycle/MainActivity.java
+++ ListViewsAndLifeCycle/app/src/main/java/com/example/listviewsandlifecycle/MainActivity.java
@@ -12,7 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
 
             LayoutInflater layoutInflater = (LayoutInflater)parentContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);  //sets layout on the screeen
             View view = layoutInflater.inflate(R.layout.adapter_custom,null);
+
+            TextView textView = view.findViewById(R.id.id_adapter_text);
+            Button button = view.findViewById(R.id.id_adapter_button);
 
             return view;
         }
