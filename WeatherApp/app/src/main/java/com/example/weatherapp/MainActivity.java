56cc968de4c1814290904b4fdf706b7e8e69cf99
RishiShah200 <36748900+RishiShah200@users.noreply.github.com>
2020-01-09 14:22:25 -0500

    Highweather works

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.stream.Collectors;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +33,16 @@ public class MainActivity extends AppCompatActivity {
     String zipcode;
     Button submitzipcode;
 
-    String currentTemp = "";
+
+    TextView currentTemp;
+
+    TextView day1maxtemp;
+    TextView day2maxtemp;
+    TextView day3maxtemp;
+    TextView day4maxtemp;
+    TextView day5maxtemp;
+
+    DecimalFormat df = new DecimalFormat("#,###,##0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
         zipCodeFinder = findViewById(R.id.id_zipcode);
         submitzipcode = findViewById(R.id.id_submitzipcode);
+
+        currentTemp = findViewById(R.id.id_currenttemp);
+
+        day1maxtemp = findViewById(R.id.day1maxtemp);
+        day2maxtemp = findViewById(R.id.day2maxtemp);
+        day3maxtemp = findViewById(R.id.day3tmaxemp);
+        day4maxtemp = findViewById(R.id.day4maxtemp);
+        day5maxtemp = findViewById(R.id.day5maxtemp);
+
 
         submitzipcode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +76,8 @@ public class MainActivity extends AppCompatActivity {
             zipcode = params[0];
 
             try{
-                url = new URL("https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode + "&appid=ece003293f26ba768ee74719308fc712");
+                url = new URL("https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode + "&appid=ece003293f26ba768ee74719308fc712");   //5 day
+               // url = new URL("http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=ece003293f26ba768ee74719308fc712");
                 connection = url.openConnection();
                 stream = connection.getInputStream();
                 streamReader = new InputStreamReader(stream);
@@ -82,13 +98,22 @@ public class MainActivity extends AppCompatActivity {
             try{
                 Log.d("INFORMATION",info);
                 JSONObject weatherData = new JSONObject(info);
-                currentTemp = weatherData.getJSONArray("list").getJSONObject(1).getJSONObject("main").getDouble("temp") + "";
-                Log.d("WEATHER",currentTemp);
+               // currentTemp.setText(weatherData.getJSONArray("list").getJSONObject(1).getJSONObject("main").getDouble("temp") + "");
+                day1maxtemp.setText("High: " + df.format(convertTemp(weatherData.getJSONArray("list").getJSONObject(1).getJSONObject("main").getDouble("temp"))) + "°F");
+                day2maxtemp.setText("High: " + df.format(convertTemp(weatherData.getJSONArray("list").getJSONObject(2).getJSONObject("main").getDouble("temp"))) + "°F");
+                day3maxtemp.setText("High: " + df.format(convertTemp(weatherData.getJSONArray("list").getJSONObject(3).getJSONObject("main").getDouble("temp"))) + "°F");
+                day4maxtemp.setText("High: " + df.format(convertTemp(weatherData.getJSONArray("list").getJSONObject(4).getJSONObject("main").getDouble("temp"))) + "°F");
+                day5maxtemp.setText("High: " + df.format(convertTemp(weatherData.getJSONArray("list").getJSONObject(5).getJSONObject("main").getDouble("temp"))) + "°F");
+             //   currentTemp.setText(weatherData.getJSONObject("main").getDouble("temp")+ "");
 
             }catch (Exception e){
 
             }
         }
+    }
+
+    public double convertTemp(double weather){
+        return (weather - 273.15) * 9/5 + 32;
     }
 
 }
