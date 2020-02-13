a673614ec42d3b205ae0854c9457cf41e743cfb7
Rishi Shah <rishishah200@gmail.com>
2020-02-13 13:39:04 -0500

    CookieaclickerUpdate

--- CookieClickerProject/app/src/main/java/com/example/cookieclickerproject/MainActivity.java
+++ CookieClickerProject/app/src/main/java/com/example/cookieclickerproject/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.cookieclickerproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,9 +17,19 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
-    TextView scoreView;
+    TextView btcView;
 
-    static AtomicInteger score;
+    static AtomicInteger btc;
+
+    CardView ramupgrade;
+
+    int ramUpgradeCost;
+
+    static int ramMultiplier = 0;
+
+    static boolean RAMbought = false;
+
+    Button startMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +38,39 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.basketball);
 
-        scoreView = findViewById(R.id.score);
+        startMenu = findViewById(R.id.startMenu);
 
-        score = new AtomicInteger();
+        startMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SideMenu.class);
+                startActivity(intent);
+            }
+        });
 
+        btcView = findViewById(R.id.score);
+
+        btc = new AtomicInteger();
+
+        ramupgrade = findViewById(R.id.ramupgrade);
+
+        ramUpgradeCost = 50;
+
+        if(btc.intValue() > ramUpgradeCost){
+            ramupgrade.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RAMbought = true;
+                    ramMultiplier = 1;
+                    btc.addAndGet(-ramUpgradeCost);
+                    btcView.setText(btc.toString() + "Ƀ");
+                }
+            });
+        }
+
+        if(RAMbought){
+            new MyThread().start();
+        }
 
         final ScaleAnimation scaleAnimation = new ScaleAnimation(1.25f,1.0f,1.25f,1.0f, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         scaleAnimation.setDuration(250);
@@ -37,9 +79,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.startAnimation(scaleAnimation);
-                score.addAndGet(1);
-                scoreView.setText(score.toString());
+                btc.addAndGet(1);
+                btcView.setText(btc.toString() + "Ƀ");
             }
         });
     }
+
+    public static class MyThread extends Thread{
+        public void run(){
+            try{
+                Thread.sleep(2);
+            }catch(Exception e){ }
+           btc.addAndGet(ramMultiplier);
+
+        }
+    }
+
 }
