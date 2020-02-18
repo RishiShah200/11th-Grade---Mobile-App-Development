28681f57a496558e417266d1bada544418bdb6d4
Rishi Shah <rishishah200@gmail.com>
2020-02-17 20:39:07 -0500

    converted back to one screen

--- CookieClickerProject/app/src/main/java/com/example/cookieclickerproject/MainActivity.java
+++ CookieClickerProject/app/src/main/java/com/example/cookieclickerproject/MainActivity.java
@@ -6,14 +6,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.animation.ObjectAnimator;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,46 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
     public static AtomicInteger btc;
 
-    CardView ramupgrade;
-    int ramUpgradeCost = 50;
-    static int ramMultiplier = 1;
+    static CardView ramupgrade;
+    static AtomicInteger ramUpgradeCost;
+    static AtomicInteger ramMultiplier;
     ObjectAnimator objectAnimator;
 
     ConstraintLayout constraintLayout;
     TextView textView;
 
-    Button storeMenu;
+    static TextView ramcnt;
+    static AtomicInteger numOfRamUpgradesBought;
 
-    static int passiveIncomeTime;
+    static AtomicInteger passiveIncome;
+    static TextView costoframupgrade;
 
-    protected void onStart() {
-        super.onStart();
-        Log.d("tag","Start");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("tag","Stop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("tag","Destroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("tag","Pause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("tag","Resume");
-    }
+    static Handler handler = new Handler();
 
     @Override
     public void onTopResumedActivityChanged(boolean isTopResumedActivity) {
@@ -78,35 +55,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Bitcoin Clicker");
-
-        passiveIncomeTime = 2000;
-
-        storeMenu = findViewById(R.id.storeMenu);
-
+        costoframupgrade = findViewById(R.id.costoframupgrade);
+        ramcnt = findViewById(R.id.ramcnt);
+        ramupgrade = findViewById(R.id.ramupgrade);
         imageView = findViewById(R.id.basketball);
-
         btcView = findViewById(R.id.score);
 
+        ramUpgradeCost = new AtomicInteger();
+        passiveIncome = new AtomicInteger();
+        ramMultiplier = new AtomicInteger();
+        numOfRamUpgradesBought = new AtomicInteger();
         btc = new AtomicInteger();
 
-        final ScaleAnimation scaleAnimation = new ScaleAnimation(1.25f,1.0f,1.25f,1.0f, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        scaleAnimation.setDuration(250);
+        ramUpgradeCost.set(50);
+        passiveIncome.set(0);
+        ramMultiplier.set(1);
 
+        final ScaleAnimation scaleAnimation = new ScaleAnimation(1.25f, 1.0f, 1.25f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(250);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                checkTransactionValidity();
                 v.startAnimation(scaleAnimation);
-                btc.addAndGet(ramMultiplier);
+                btc.addAndGet(ramMultiplier.get());
                 btcView.setText(btc.toString() + "Ƀ");
 
                 constraintLayout = findViewById(R.id.id_layout);
                 textView = new TextView(MainActivity.this);
                 textView.setId(View.generateViewId());
 
-                textView.setText(ramMultiplier+"");
+                textView.setText(ramMultiplier + "");
                 ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT
-                        ,ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                        , ConstraintLayout.LayoutParams.WRAP_CONTENT);
 
                 textView.setLayoutParams(params);
                 constraintLayout.addView(textView);
@@ -114,91 +96,84 @@ public class MainActivity extends AppCompatActivity {
                 ConstraintSet constraintSet = new ConstraintSet();  //whatever constraints were already present will be cloned.
                 constraintSet.clone(constraintLayout);
 
-                constraintSet.connect(textView.getId(),ConstraintSet.TOP,constraintLayout.getId(),ConstraintSet.TOP);
-                constraintSet.connect(textView.getId(),ConstraintSet.BOTTOM,constraintLayout.getId(),ConstraintSet.BOTTOM);
-                constraintSet.connect(textView.getId(),ConstraintSet.LEFT,constraintLayout.getId(),ConstraintSet.LEFT);
-                constraintSet.connect(textView.getId(),ConstraintSet.RIGHT,constraintLayout.getId(),ConstraintSet.RIGHT);
+                constraintSet.connect(textView.getId(), ConstraintSet.TOP, constraintLayout.getId(), ConstraintSet.TOP);
+                constraintSet.connect(textView.getId(), ConstraintSet.BOTTOM, constraintLayout.getId(), ConstraintSet.BOTTOM);
+                constraintSet.connect(textView.getId(), ConstraintSet.LEFT, constraintLayout.getId(), ConstraintSet.LEFT);
+                constraintSet.connect(textView.getId(), ConstraintSet.RIGHT, constraintLayout.getId(), ConstraintSet.RIGHT);
 
-                float rand = (float)(Math.random()*.5)+.35f;
-                constraintSet.setHorizontalBias(textView.getId(),rand);
-                constraintSet.setVerticalBias(textView.getId(),0.3f);
+                float rand = (float) (Math.random() * .7) + .15f;
+                constraintSet.setHorizontalBias(textView.getId(), rand);
+                constraintSet.setVerticalBias(textView.getId(), 0.3f);
 
                 constraintSet.applyTo(constraintLayout);
 
-                objectAnimator = ObjectAnimator.ofFloat(textView,"translationY",-600f);
-                objectAnimator.setDuration(1000);
+                objectAnimator = ObjectAnimator.ofFloat(textView, "translationY", -600f);
+                objectAnimator.setDuration(2000);
+
                 objectAnimator.start();
 
             }
         });
 
-        storeMenu.setOnClickListener(new View.OnClickListener() {
+        ramupgrade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startStore = new Intent(MainActivity.this,StoreActivity.class);
-                startStore.putExtra("ramUpgradeCost",ramUpgradeCost);
-                startStore.putExtra("ramMultiplier",ramMultiplier);
-                startStore.putExtra("passiveIncomeTime",passiveIncomeTime);
-                startStore.putExtra("BTC",btc.intValue());
-                startActivity(startStore);
+                numOfRamUpgradesBought.addAndGet(1);
+                passiveIncome.addAndGet(1);
+                ramcnt.setText(numOfRamUpgradesBought + "");
+                ramMultiplier.addAndGet(1);
+                btc.addAndGet(-1 * ramUpgradeCost.get());
+                ramUpgradeCost.set((ramUpgradeCost.get() * 2));
+                costoframupgrade.setText(ramUpgradeCost.get() + "");
             }
         });
 
-        ramUpgradeCost = getIntent().getIntExtra("ramUpgradeCostAfterStore",50);
-        ramMultiplier = getIntent().getIntExtra("ramMultiplierAfterStore",1);
-        passiveIncomeTime = getIntent().getIntExtra("passiveIncomeTime",2000);
-        btc.set(getIntent().getIntExtra("BTC",0));
-        btcView.setText(btc.toString() + "Ƀ");
-
         if (savedInstanceState != null) {
-            Log.d("BTCVALUE","TEST");
-            Log.d("BTCVALUE",savedInstanceState.getInt("BTC")+"");
             btc.set(savedInstanceState.getInt("BTC"));
             btcView.setText(btc.toString() + "Ƀ");
-            ramMultiplier = savedInstanceState.getInt("ramMultiplier");
-            ramUpgradeCost = savedInstanceState.getInt("ramUpgradeCost");
-            passiveIncomeTime = savedInstanceState.getInt("passiveIncomeTime");
+            ramMultiplier.set(savedInstanceState.getInt("ramMultiplier"));
+            ramUpgradeCost.set(savedInstanceState.getInt("ramUpgradeCost"));
+            passiveIncome.set(savedInstanceState.getInt("passiveIncome"));
         }
 
-
         btcView.setText(btc.toString() + "Ƀ");
+        handler.postDelayed(new MyThread(), 1000);
 
     }
 
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        Log.d("BTCVALUE",btc.intValue()+"");
-        outState.putInt("BTC",btc.intValue());
-        outState.putInt("ramUpgradeCost",ramUpgradeCost);
-        outState.putInt("ramMultiplier",ramMultiplier);
-        outState.putInt("passiveIncomeTime",passiveIncomeTime);
+        outState.putInt("BTC", btc.get());
+        outState.putInt("ramUpgradeCost", ramUpgradeCost.get());
+        outState.putInt("ramMultiplier", ramMultiplier.get());
+        outState.putInt("passiveIncome", passiveIncome.get());
     }
 
-    public static class MyThread extends Thread{
-        Handler handler = new Handler();
+    public static class MyThread extends Thread {
 
-        public void run(){
-            try{
-                Thread.sleep(2);
-            }catch(Exception e){ }
-            while(true) {
-                btc.addAndGet(ramMultiplier);
-                try {
-                    Thread.sleep(passiveIncomeTime);
-                    Log.d("passiveIncomeTime",passiveIncomeTime+"");
-                } catch (Exception e) {
 
-                }
-
-                handler.post(new Runnable() {
-                    public void run() {
-                        btcView.setText(btc.toString() + "Ƀ");
-                    }
-                });
+        public void run() {
+            try {
+                checkTransactionValidity();
+                btc.addAndGet(passiveIncome.get());
+                btcView.setText(btc.get() + "Ƀ");
+            } catch (Exception e) {
 
             }
+            Log.d("BTC",(btc.get() >= ramUpgradeCost.get())+"");
+            handler.postDelayed(this, 1000);
+        }
+    }
 
+    public static void checkTransactionValidity() {
+        if (btc.get() >= ramUpgradeCost.get()) {
+            ramupgrade.setClickable(true);
+            ramupgrade.setCardBackgroundColor(Color.RED);
+        }
+        else if (btc.get() <= ramUpgradeCost.get()) {
+            ramupgrade.setClickable(false);
+            ramupgrade.setCardBackgroundColor(Color.GRAY);
         }
     }
 
